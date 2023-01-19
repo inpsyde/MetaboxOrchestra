457fb5ef00f38741fa10534b369ed8af6b425121
Alex P <alex.pantec@gmail.com>
2023-01-19 09:41:38 +0200

    Update deps, require php 7.2

--- tests/src/TestCase.php
+++ tests/src/TestCase.php
@@ -18,14 +18,14 @@ use Brain\Monkey;
  */
 class TestCase extends \PHPUnit\Framework\TestCase {
 
-	protected function setUp() {
+	protected function setUp(): void {
 
 		parent::setUp();
 		Monkey\setUp();
 
 	}
 
-	protected function tearDown() {
+	protected function tearDown(): void {
 
 		Monkey\tearDown();
 		parent::tearDown();
